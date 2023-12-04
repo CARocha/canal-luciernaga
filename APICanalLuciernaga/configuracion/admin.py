a71feeaa5c1ba5f2cc317a400b7718a2871e1273
Carlos Rocha <crocha09.09@gmail.com>
2023-12-03 19:41:12 -0600

    agregando las imagenes que van a ir el banner y su migracion

--- APICanalLuciernaga/configuracion/admin.py
+++ APICanalLuciernaga/configuracion/admin.py
@@ -3,4 +3,17 @@ from .models import *
 from solo.admin import SingletonModelAdmin
 
 # Register your models here.
+class InlinesImagesInline(admin.TabularInline):
+    model = InlinesImages
+    extra = 1
+
+class BannerSitioAdmin(SingletonModelAdmin):
+    inlines = (InlinesImagesInline,)
+    list_display = ('titulo','tipo')
+    list_filter = ('tipo',)
+    
+    class Media:
+        js = ('js/admin/banner.js',)
+
 admin.site.register(ConocenosImg,SingletonModelAdmin)
+admin.site.register(BannerSitio,BannerSitioAdmin)
--- APICanalLuciernaga/configuracion/models.py
+++ APICanalLuciernaga/configuracion/models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from django.db import models
 from solo.models import SingletonModel
 from sorl.thumbnail import ImageField
@@ -9,3 +10,42 @@ class ConocenosImg(SingletonModel):
 
 	class Meta:
 		verbose_name = "Imágen conócenos"
+
+
+# class Opciones(Enum):
+#     IMAGEN = "IMAGEN"
+#     VIDEO = "VIDEO"
+#     YOUTUBE = "YOUTUBE"
+
+#     @classmethod
+#     def choices(cls):
+#         return tuple((obj.name, obj.value) for obj in cls)
+
+choices = ((1, 'IMAGEN'), (2, 'YOUTUBE'))
+
+class BannerSitio(SingletonModel):
+    tipo = models.IntegerField(choices=choices, default=1)
+    titulo = models.CharField(max_length=250, blank=True, null=True)
+    descripcion = models.TextField(null=True,blank=True)
+    youtube_link = models.CharField(max_length=250,
+                                    null=True,
+                                    blank=True)
+    def __str__(self):
+        return str(self.tipo)
+    class Meta:
+        verbose_name = "Banner sitio"
+        verbose_name_plural = "Banner sitio"
+class InlinesImages(models.Model):
+    banner = models.ForeignKey('BannerSitio', on_delete=models.CASCADE)
+    titulo_img = models.CharField(max_length=250)
+    descripcion_img = models.TextField(null=True,blank=True)
+    imagen = ImageField(upload_to='banner/conocenos',
+                        help_text='1350x800',
+                        null=True,
+                        blank=True)
+
+    def __str__(self):
+        return self.titulo_img
+    class Meta:
+        verbose_name = "Imagenes"
+        verbose_name_plural = "Imagenes"
