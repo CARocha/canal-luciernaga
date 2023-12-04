c8742f2b3e564e458c14ed98c8f9bb443fced5c4
Carlos Rocha <crocha09.09@gmail.com>
2023-12-03 19:40:00 -0600

    agregando mejoras a la bibliotecas

--- APICanalLuciernaga/biblioteca/admin.py
+++ APICanalLuciernaga/biblioteca/admin.py
@@ -7,6 +7,12 @@ class CategoriaAdmin(admin.ModelAdmin):
 
 class BibliotecaAdmin(admin.ModelAdmin):
     autocomplete_fields = ['categoria']
+    list_display = ['nombre',
+                    'categoria',
+                    'image_tag',
+                    'archivo']
+    list_filter = ('categoria__nombre',)
+    search_fields = ['nombre']
 
 admin.site.register(Biblioteca,BibliotecaAdmin)
 # admin.site.register(Categoria,CategoriaAdmin)
--- APICanalLuciernaga/biblioteca/models.py
+++ APICanalLuciernaga/biblioteca/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 from sorl.thumbnail import ImageField
 from noticias.models import Categoria
+from sorl.thumbnail import ImageField
+from django.utils.safestring import mark_safe
+from noticias.models import Categoria
 # Create your models here.
 
 
 class Biblioteca(models.Model):
-    nombre = models.CharField(max_length = 225)
-    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
-    descripcion = models.TextField(max_length = 255)
-    foto = ImageField('Imagen',upload_to='fotos/biblioteca')
+    nombre = models.CharField(max_length=225)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.TextField(max_length=255)
+    foto = ImageField('Imagen', upload_to='fotos/biblioteca')
     archivo = models.FileField(upload_to='documento/biblioteca')
 
     def __str__(self):
         return self.nombre
-    
+
+    def image_tag(self):
+        return mark_safe('<img src="/media/%s" width="150" height="150" />' % (self.foto))
+
+    image_tag.short_description = 'Imagen'
     class Meta:
         verbose_name = "Biblioteca"
-        verbose_name_plural = "Biblioteca"
+        verbose_name_plural = "Bibliotecas"
