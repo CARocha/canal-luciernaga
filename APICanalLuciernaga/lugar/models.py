92a2e9d28eacdd8bb072d40016b58854cf801292
Carlos Rocha <crocha09.09@gmail.com>
2024-07-02 21:56:25 -0600

    feat: Add unique constraint to Pais model's nombre field

--- APICanalLuciernaga/lugar/models.py
+++ APICanalLuciernaga/lugar/models.py
@@ -1,19 +1,25 @@
 # Description: Modelo de la tabla Pais
 from django.db import models
-# from django.utils.text import slugify
+from django.core.exceptions import ValidationError
+from django.db.models.functions import Lower
 
 # Create your models here.
 
 class Pais(models.Model):
-    nombre = models.CharField(max_length = 225)
+    nombre = models.CharField(max_length = 225, unique=True)
     slug = models.SlugField(max_length = 225, unique=True, editable=True)
 
     def __str__(self):
         return self.nombre
 
-    # def save(self, *args, **kwargs):
-    #     self.slug = slugify(self.nombre)
-    #     super(Pais, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        self.nombre = self.nombre.lower()
+        super(Pais, self).save(*args, **kwargs)
+
+    def clean(self):
+        if Pais.objects.annotate(nombre_lower=Lower('nombre')).filter(nombre_lower=self.nombre.lower()).exists():
+            raise ValidationError({'nombre': 'Un país con este nombre ya existe.'})
+        super(Pais, self).clean()
 
     class Meta:
         verbose_name = "Pais"
