0f47b1b01d9ecc8ff24797de38ad928f65e36acf
Carlos Rocha <crocha09.09@gmail.com>
2024-07-02 21:21:15 -0600

    Add validation to prevent duplicate country names in PaisAdmin

--- APICanalLuciernaga/lugar/admin.py
+++ APICanalLuciernaga/lugar/admin.py
@@ -5,9 +5,18 @@ class PaisAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         if Pais.objects.filter(nombre=obj.nombre).exists() and not change:
             messages.error(request, f"El país '{obj.nombre}' ya existe.")
+            return
         else:
-            obj.save()
+            super().save_model(request, obj, form, change)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(PaisAdmin, self).get_form(request, obj, **kwargs)
+        if form and 'slug' in form.base_fields:
+            form.base_fields['slug'].widget.attrs['readonly'] = True  # Hace el campo slug de solo lectura en el admin
+        return form
+
     search_fields = ['nombre']
+    prepopulated_fields = {"slug": ("nombre",)}
 
 
 # Register your models here.
--- APICanalLuciernaga/lugar/models.py
+++ APICanalLuciernaga/lugar/models.py
@@ -1,19 +1,19 @@
 # Description: Modelo de la tabla Pais
 from django.db import models
-from django.utils.text import slugify
+# from django.utils.text import slugify
 
 # Create your models here.
 
 class Pais(models.Model):
     nombre = models.CharField(max_length = 225)
-    slug = models.SlugField(max_length = 225, unique=True, editable=False)
+    slug = models.SlugField(max_length = 225, unique=True, editable=True)
 
     def __str__(self):
         return self.nombre
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.nombre)
-        super(Pais, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.nombre)
+    #     super(Pais, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Pais"
