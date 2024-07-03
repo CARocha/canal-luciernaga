73106e7a5c6386ba4c4ed72a7ac8432ee375fd49
Carlos Rocha <crocha09.09@gmail.com>
2024-07-02 20:54:58 -0600

    Add validation to prevent duplicate country names in PaisAdmin

--- APICanalLuciernaga/lugar/admin.py
+++ APICanalLuciernaga/lugar/admin.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import *
 
 class PaisAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if Pais.objects.filter(nombre=obj.nombre).exists() and not change:
+            messages.error(request, f"El país '{obj.nombre}' ya existe.")
+        else:
+            obj.save()
     search_fields = ['nombre']
 
 
--- APICanalLuciernaga/lugar/models.py
+++ APICanalLuciernaga/lugar/models.py
@@ -6,14 +6,14 @@ from django.utils.text import slugify
 
 class Pais(models.Model):
     nombre = models.CharField(max_length = 225)
-    slug = models.SlugField(max_length = 225)
+    slug = models.SlugField(max_length = 225, unique=True, editable=False)
 
     def __str__(self):
         return self.nombre
 
-    # def save(self, *args, **kwargs):
-    #     self.slug = slugify(self.nombre)
-    #     super(Pais, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nombre)
+        super(Pais, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Pais"
