291b8fd9a667cb747774cc16833bd37616b212e7
Carlos Rocha <crocha09.09@gmail.com>
2024-07-09 23:56:55 -0600

    Add TiposFilter forms for filter views

--- APICanalLuciernaga/videoteca/forms.py
+++ APICanalLuciernaga/videoteca/forms.py
@@ -7,9 +7,17 @@ def categorias(tipo):
 	foo = Video.objects.filter(tipo__slug=tipo).values_list('categoria',flat=True)
 	return Categoria.objects.filter(id__in = foo)
 
+def tipos(categoria):   
+	foo = Video.objects.filter(categoria__slug=categoria).values_list('tipo',flat=True)
+	return Tipo.objects.filter(id__in = foo)
+
 class CategoriasFilter(forms.Form):
 	def __init__(self, *args, **kwargs):
 		tipo = kwargs.pop("tipo")
 		super(CategoriasFilter, self).__init__(*args, **kwargs)
 		self.fields['categorias'] = forms.ModelChoiceField(queryset=categorias(tipo), required=True, label=u'Categorias')
-	
+class TiposFilter(forms.Form):
+	def __init__(self, *args, **kwargs):
+		categoria = kwargs.pop("categoria")
+		super(TiposFilter, self).__init__(*args, **kwargs)
+		self.fields['tipos'] = forms.ModelChoiceField(queryset=tipos(categoria), required=True, label=u'Tipos')
