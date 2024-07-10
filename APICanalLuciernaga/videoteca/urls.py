ad141e72d479c7b0fa99c0e1a5d9ab261d37a66f
Carlos Rocha <crocha09.09@gmail.com>
2024-07-09 23:57:18 -0600

    Update urls video for more clarification

--- APICanalLuciernaga/videoteca/urls.py
+++ APICanalLuciernaga/videoteca/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('<tipo>/',list_videos, name='list_videos'),
+	path('tipo/<slug:tipo>/',list_videos, name='list_videos'),
+    path('categoria/<slug:categoria>/',list_videos_categoria, name='list_videos_categoria'),
 	path('detalle/<slug>/',Video_detail,name='Video_detail'),
-	path('detalle/<slug>/temporada/<temporada>/episodio/<episodio>/',episodio_detail,name='episodio_detail'),
+	path('episodio/<slug:episodio>/temporada/<int:temporada>/',episodio_detail,name='episodio_detail'),
 	path('ajax/video_info/',GetVideoInfo,name='get_video_info'),
 ]
