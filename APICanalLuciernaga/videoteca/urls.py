40583cda4ffb05cd337a5e31446cb31042948f89
Carlos Rocha <crocha09.09@gmail.com>
2024-07-17 21:37:29 -0600

    Add detail_video endpoint for video detail page

--- APICanalLuciernaga/videoteca/urls.py
+++ APICanalLuciernaga/videoteca/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 	path('tipo/<slug:tipo>/',list_videos, name='list_videos'),
     path('categoria/<slug:categoria>/',list_videos_categoria, name='list_videos_categoria'),
 	path('detalle/<slug>/',Video_detail,name='Video_detail'),
+    path('video/<slug>/',detail_video,name='detail_video'),
 	path('episodio/<slug:episodio>/temporada/<int:temporada>/',episodio_detail,name='episodio_detail'),
 	path('ajax/video_info/',GetVideoInfo,name='get_video_info'),
 ]
